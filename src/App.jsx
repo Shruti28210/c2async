65565c185598c6425855c749ef6b64286c86c936
Shruti Waghmode <waghmodeshru@gmail.com>
2022-04-18 16:04:14 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import axios from "axios";
 import { AddStudent } from "./components/AddStudent";
 import { ShowStudents } from "./components/ShowStudents";
+import './App.css'
 
 function App() {
+  const [box,setBox] = React.useState(false)
   const [data,setData] = React.useState([]);
 
   const fetchdata = () =>{
@@ -19,9 +21,10 @@ function App() {
 
   return (
     <div className="App">
-      <button className="togglebtn">Toggle</button>
-      <AddStudent fetchdata={fetchdata}/>
-      <ShowStudents data = {data}/>
+      <button className="togglebtn" onClick={() => setBox(!box)}>{box ? "Go To Students list" : "Add aNew Student"}</button>
+      {box ? <AddStudent fetchdata={fetchdata}/> : <ShowStudents data = {data}/>}
+      {/* <AddStudent fetchdata={fetchdata}/>
+      <ShowStudents data = {data}/> */}
       {/* Show either  AddStudent component or ShowStudents dependeing on the above button click  */}
       {/* make sure the table is shown initially, do not show form initially */}
       {/* make sure to show either of them do not both together */}
